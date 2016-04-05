a1ab774f4e7d3e0fe27bd4eb0fb31f9bbb645fbe
Peter F. Klemperer <peter@peterklemperer.com>
2016-04-05 10:32:49 -0400

    another change to hello world. added another write

--- src/hello.java
+++ src/hello.java
@@ -2,6 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         
-        System.out.println("hoepfully this also works.")
+        System.out.println("hopefully this also works.");
     }
 }
